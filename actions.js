3a27cc4bb501663715cf13e7d850cc59dbdb7ad5
Aldo Aníbal Colombo <44879977+acolombo1@users.noreply.github.com>
2022-04-13 19:57:08 -0300

    add html and css for full main page

--- actions.js
+++ actions.js
@@ -31,3 +31,23 @@ const projects = {
     linkSource: '#',
   },
 };
+
+const btnHamburger = document.getElementById('hamburger-menu');
+const mobileMenu = document.getElementById('mobile-menu');
+const btnCloseMenu = document.getElementById('close-menu');
+const menuItem = document.querySelectorAll('.list-item');
+
+/* Events and their actions */
+btnHamburger.addEventListener('click', () => {
+  mobileMenu.style.display = 'block';
+});
+
+btnCloseMenu.addEventListener('click', () => {
+  mobileMenu.style.display = 'none';
+});
+
+for (let i = 0; i < menuItem.length; i++) {
+  menuItem[i].addEventListener('click', () => {
+    mobileMenu.style.display = 'none';
+  });
+}
